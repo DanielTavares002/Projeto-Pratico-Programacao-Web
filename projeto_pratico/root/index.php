42e33d3aa7d11d208a2f5294b48ed40ffcca21d9
DanielTavares002 <danieltavaresdossantosdt@gmail.com>
2025-11-07 16:31:51 -0300

    Implementa conexão PDO

---
+++ projeto_pratico/root/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset='utf-8'>
+    <title>Projeto</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='style.css'>
+    <script src='main.js'></script>
+    
+</head>
+<body>
+    <p>Sim</p>
+</body>
+</html>
