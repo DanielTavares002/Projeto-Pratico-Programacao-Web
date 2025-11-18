db3e29fa9480c43e26bf40daed02f43fbb4e588a
Jose Gustavo Martinho <gustavomartinho11@gmail.com>
2025-11-18 15:07:00 -0300

    Update index.php

--- projeto_pratico/root/index.php
+++ projeto_pratico/root/index.php
@@ -1,13 +1,49 @@
+<?php
+session_start();
+require 'conexao.php';
+
+if($_SERVER['REQUEST_METHOD'] === 'POST'){
+    $email = $_POST['email'];
+    $senha = $_POST['senha'];
+
+    $stmt = $pdo->prepare("SELECT * FROM usuarios WHERE email = ?");
+    $stmt->execute([$email]);
+    $usuario = $stmt->fetch();
+
+    if($usuario && password_verify($senha, $usuario['senha'])){
+        $_SESSION['usuario_id'] = $usuario['id'];
+        $_SESSION['usuario_nome'] = $usuario['nome'];
+        header("Location: dashboard.php");
+        exit;
+    } else {
+        $erro = "Email ou senha incorretos!";
+    }
+}
+?>
+
 <!DOCTYPE html>
-<html lang="pt-BR">
+<html lang="pt-br">
 <head>
-    <meta charset='utf-8'>
-    <title>Projeto</title>
-    <meta name='viewport' content='width=device-width, initial-scale=1'>
-    <link rel='stylesheet' type='text/css' media='screen' href='style.css'>
-    <script src='main.js'></script>
-    
+    <meta charset="UTF-8">
+    <title>Login</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
+<h2>Login</h2>
+<div class="container">
+    <form method="POST" class="box">
+        <?php if(isset($erro)) echo "<p style='color:red; text-align:center;'>$erro</p>"; ?>
+        
+        <label>Email:</label><br>
+        <input type="email" name="email" required><br><br>
+
+        <label>Senha:</label><br>
+        <input type="password" name="senha" required><br><br>
+
+        <button type="submit">Login</button><br>
+        <p><a href="registro.php">Não possui conta? Cadastre-se</a></p>
+    </form>
+</div>
+
 </body>
 </html>
